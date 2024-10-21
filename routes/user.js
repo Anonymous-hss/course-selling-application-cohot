fd687a0fff5e3a942703365763a5c25fdf7370a6
Anonymous-hss <harshalsawatkar24@gmail.com>
2024-10-21 22:58:56 +0530

    rm node modules

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,30 @@
 const { Router } = require("express");
+const { bcrypt } = require("bcrypt");
+const saltRounds = 5;
+const { userModel } = require("../db");
 
 const userRouter = Router();
 
-userRouter.post("/signin", function (req, res) {
+userRouter.post("/signin", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  } catch (e) {
+    message: "Error encrypting password";
+  }
+
+  try {
+    await userModel.create({
+      email: email,
+      hashedPassword: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } catch (e) {
+    message: "Error connecting DB" + e;
+  }
+
   res.json({
     message: "login endpoint",
   });
