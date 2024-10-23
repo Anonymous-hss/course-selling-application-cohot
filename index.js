73b1e4ce396da9daf38551416197230d791150a4
Anonymous-hss <harshalsawatkar24@gmail.com>
2024-10-24 00:24:15 +0530

    signup created

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const mongoose = requrie("mongoose");
+const mongoose = require("mongoose");
 const { courseRouter } = require("./routes/courses");
 const { userRouter } = require("./routes/user");
 const { adminRouter } = require("./routes/admin");
 
 const app = express();
+app.use(express.json());
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/courses", courseRouter);
@@ -12,16 +13,14 @@ app.use("/courses", courseRouter);
 async function main() {
   await mongoose
     .connect(
-      "mongodb+srv://h2424:h2424@cluster0.c2mlnwt.mongodb.net/courseraapp",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://h2424:h2424@cluster0.c2mlnwt.mongodb.net/courseraapp"
     )
     .then(() => console.log("MongoDB connected successfully"))
     .catch((err) => console.log("MongoDB connection error:", err));
 
-  app.listen(3000);
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
 }
 
 main();
--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,34 @@
 const { Router } = require("express");
-const { bcrypt } = require("bcrypt");
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 const saltRounds = 5;
 const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "aladin4568";
 
 const userRouter = Router();
 
-userRouter.post("/signin", async function (req, res) {
-  const { email, password, firstName, lastName } = req.body;
+const userSchema = z.object({
+  email: z.string().email({ message: "Invalid Emial Address" }),
+  password: z.string().min(6, { message: "Enter min of 6 characters" }),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
 
+userRouter.post("/signup", async function (req, res) {
   try {
+    const { email, password, firstName, lastName } = userSchema.parse(req.body);
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-  } catch (e) {
-    message: "Error encrypting password";
-  }
-
-  try {
     await userModel.create({
       email: email,
       hashedPassword: hashedPassword,
       firstName: firstName,
       lastName: lastName,
     });
+    res.json({ message: "created successfully" });
   } catch (e) {
-    message: "Error connecting DB" + e;
+    console.error(e); // This will log the actual error in the server console
+    return res.status(500).json({ message: "Server error" });
   }
 
   res.json({
@@ -30,7 +36,32 @@ userRouter.post("/signin", async function (req, res) {
   });
 });
 
-userRouter.post("/signup", function (req, res) {
+userRouter.post("/signin", async function (req, res) {
+  const { email, passord } = res.body;
+
+  const user = await userModel.find({
+    email: email,
+    password: password,
+  });
+
+  if (user) {
+    jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_USER_PASSWORD
+    );
+    // Token based authentication logic -----
+
+    res.json({
+      message: "signup endpoint",
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
+
   res.json({
     message: "registration endpoint",
   });
