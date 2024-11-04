7aabd8895c62310c11611db9cc6f799379997675
Anonymous-hss <harshalsawatkar24@gmail.com>
2024-11-04 23:56:58 +0530

    course endpoints updated

--- routes/courses.js
+++ routes/courses.js
@@ -1,16 +1,27 @@
+const { purchaseModel, courseModel } = require("../db");
 const { Router } = require("express");
 
 const courseRouter = Router();
 
-courseRouter.post("/purchase", function (req, res) {
+courseRouter.post("/purchase", async function (req, res) {
+  const userId = req.userId;
+  const courseId = req.body.courseId;
+
+  await purchaseModel.create({
+    userId,
+    courseId,
+  });
+
   res.json({
-    message: "purchase",
+    message: "You have sucessfully bought the course",
   });
 });
 
-courseRouter.get("/course", function (req, res) {
+courseRouter.get("/preview", async function (req, res) {
+  const course = await courseModel.find({});
   res.json({
     message: "All courses endpoint",
+    course,
   });
 });
 
